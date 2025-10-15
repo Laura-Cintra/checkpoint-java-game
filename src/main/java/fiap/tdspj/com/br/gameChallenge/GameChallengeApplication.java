8dd4ad7fc3e68a7f952a276c40ac31df35ed842f
Laura-Cintra <lauraocintra@gmail.com>
2025-10-15 08:49:40 -0300

    feat: base project

---
+++ src/main/java/fiap/tdspj/com/br/gameChallenge/GameChallengeApplication.java
@@ -0,0 +1,13 @@
+package fiap.tdspj.com.br.gameChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/tdspj/com/br/gameChallenge/GameChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.tdspj.com.br.gameChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
