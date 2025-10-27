fdcae5a7bb4ba4684140ed85bb3442b22ae1a40a
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-10-27 10:47:24 -0300

    feat: usuarios de teste e quando logout voltar ao login

--- src/main/java/fiap/tdspj/com/br/gameChallenge/config/SecurityConfig.java
+++ src/main/java/fiap/tdspj/com/br/gameChallenge/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package fiap.tdspj.com.br.gameChallenge.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
@@ -20,7 +23,28 @@ public class SecurityConfig {
                         .loginPage("/login")
                         .permitAll()
                 )
-                .logout(logout -> logout.permitAll());
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("maria")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("laura")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
 }
